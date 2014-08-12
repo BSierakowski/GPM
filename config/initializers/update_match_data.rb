81242735f193acfd90e766bd93bf16bab2ddcca4
Brian Sierakowski <BrianSierakowski@gmail.com>
2014-08-12 17:52:17 -0400

    another check for bad data, preheating the DB

---
+++ config/initializers/update_match_data.rb
@@ -0,0 +1,8 @@
+Rails.application.config.after_initialize do
+  DotaMatchHistoryWorker.perform_async(35448985) #me
+  DotaMatchHistoryWorker.perform_async(106686450) #vo
+  DotaMatchHistoryWorker.perform_async(87316237) #john
+  DotaMatchHistoryWorker.perform_async(76911339) #zig
+  DotaMatchHistoryWorker.perform_async(6866422) #rain
+  DotaMatchHistoryWorker.perform_async(5570051) #polly
+end
