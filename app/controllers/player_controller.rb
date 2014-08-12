aa20c09f766e07d5443cb461dcba9f14597bf3eb
Brian Sierakowski <BrianSierakowski@gmail.com>
2014-08-12 16:04:06 -0400

    Put lookup of match history into a worker

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -10,39 +10,6 @@ class PlayerController < ApplicationController
 
     @stats = my_stats.sort_by! { |game| game[:gold_per_minute] }.reverse!
 
-    update_matches(player_id)
-  end
-
-  def update_matches(player_id)
-    stored_match_ids = PlayerMatch.where(player_id: player_id).map(&:match_id)
-    dota_match_ids = get_matches_from_api(player_id)
-    nonpersisted_matches = dota_match_ids - stored_match_ids
-
-    nonpersisted_matches.each do |match|
-      PlayerMatchWorker.perform_async(match)
-    end
-  end
-
-  def get_matches_from_api(player_id)
-    # Would love to get the the F out of the controller and into another worker
-    require 'open-uri'
-
-    dota_match_ids = []
-
-    first_match_page = JSON.load(open("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key=#{ENV["STEAM_API_KEY"]}&account_id=#{player_id}&matches_requested=100"))
-
-    first_match_page["result"]["matches"].each do |match|
-      dota_match_ids.push(match["match_id"])
-    end
-
-    4.times do 
-      match_page = JSON.load(open("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key=#{ENV["STEAM_API_KEY"]}&account_id=#{player_id}&matches_requested=100&start_at_match_id=#{dota_match_ids.last-1}"))
-      
-      match_page["result"]["matches"].each do |match|
-        dota_match_ids.push(match["match_id"])
-      end
-    end
-
-    return dota_match_ids
+    DotaMatchHistoryWorker.perform_async(player_id)
   end
 end
---
+++ app/workers/dota_match_history_worker.rb
@@ -0,0 +1,34 @@
+class DotaMatchHistoryWorker
+  include Sidekiq::Worker
+  require 'open-uri'
+
+  def perform(player_id)
+    stored_match_ids = PlayerMatch.where(player_id: player_id).map(&:match_id)
+    dota_match_ids = get_matches_from_api(player_id)
+    nonpersisted_matches = dota_match_ids - stored_match_ids
+
+    nonpersisted_matches.each do |match_id|
+      PlayerMatchWorker.perform_async(match_id)
+    end
+  end
+
+  def get_matches_from_api(player_id)
+    dota_match_ids = []
+
+    first_match_page = JSON.load(open("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key=#{ENV["STEAM_API_KEY"]}&account_id=#{player_id}&matches_requested=100"))
+
+    first_match_page["result"]["matches"].each do |match|
+      dota_match_ids.push(match["match_id"])
+    end
+
+    4.times do 
+      match_page = JSON.load(open("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key=#{ENV["STEAM_API_KEY"]}&account_id=#{player_id}&matches_requested=100&start_at_match_id=#{dota_match_ids.last-1}"))
+      
+      match_page["result"]["matches"].each do |match|
+        dota_match_ids.push(match["match_id"])
+      end
+    end
+
+    return dota_match_ids
+  end
+end
