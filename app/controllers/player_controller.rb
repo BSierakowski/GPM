08e2368f6a2121b9b7c69e40e75814264f818c15
Brian Sierakowski <BrianSierakowski@gmail.com>
2014-09-07 15:34:03 -0400

    filter out bot and short games, move filtering to controller

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -12,7 +12,15 @@ class PlayerController < ApplicationController
     player_matches = PlayerMatch.where(player_id: player_id)
     
     player_matches.each do |match|
-      my_stats << { match_id: match.match_id, hero: get_hero_name(match.hero_id), gold_per_minute: match.gold_per_minute, days_ago: get_days_ago(match.start_time), duration_in_minutes: get_duration_in_minutes(match.duration)}
+      if match.match_id.present?
+        if match.hero_id.present?
+          if match.human_players == 10
+            if get_duration_in_minutes(match.duration) > 10
+              my_stats << { match_id: match.match_id, hero: get_hero_name(match.hero_id), gold_per_minute: match.gold_per_minute, days_ago: get_days_ago(match.start_time), duration_in_minutes: get_duration_in_minutes(match.duration), human_players: match.human_players}
+            end
+          end
+        end
+      end
     end
 
     @stats = my_stats.sort_by! { |game| game[:gold_per_minute] }.reverse!
